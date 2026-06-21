581492eac373824828551965845ddf409f240121
nexuster <coralpinklmao@outlook.com>
2026-06-21 22:08:21 +0000

    my computer die

--- app.js
+++ app.js
@@ -1,2 +1,42 @@
-var chatmsg = document.getElementsByClass("message");
-var msgTemplate = chatmsg[0];
+const inputField = document.getElementById("msgInput");
+const sendButton = document.getElementById("send-button");
+const username = document.getElementById("nickInput");
+const msgContainer = document.getElementById("messages");
+
+function doSubmitMsg(msg,name) {
+    const text = msg.trim();
+    if (!text) return;
+
+    const newMsg = document.createElement('div');
+    newMsg.className = "message hbox";
+    newMsg.innerHTML = `
+        <img src="gfx/img/default.png"/>
+        <div class="vbox">
+            <p class="bold">${name}</p>
+            <p>${text}</p>
+        </div>
+    `;
+
+    msgContainer.appendChild(newMsg);
+}
+
+function submitCurrentMessage() {
+    const nm = username.value.trim()
+    if (!nm) {
+        alert("u must have a valid username first");
+        return
+    }
+    doSubmitMsg(inputField.value,nm);
+    inputField.value = "";
+}
+
+inputField.addEventListener('keydown', function (e) {
+    if (e.code === "Enter") {
+        e.preventDefault();
+        submitCurrentMessage();
+    }
+});
+
+if (sendButton) {
+    sendButton.addEventListener('click', submitCurrentMessage);
+}
